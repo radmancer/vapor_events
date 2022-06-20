86eedb6c8327625e09516ce4ab72189a12b7bb9d
James Scott McDowell <mcdowellj87@gmail.com>
2022-06-20 18:41:33 -0400

    Added linked list functionality to card scrolling.

--- js/swipe.js
+++ js/swipe.js
@@ -4,6 +4,19 @@ var swipeHorizontalSpeed = 2;
 var verticalSwipe = false;
 var horizontalSwipe = false;
 
+let cLL = new circularLinkedList();
+cLL.append(["css/images/knife.JPG", "You have my sword..."]);
+cLL.append(["css/images/arrow.JPG", "And you have my bow."]);
+cLL.append(["css/images/axe.JPG", "And *my* axe."]);
+cLL.append(["css/images/2.png", "yellow"]);
+cLL.append(["css/images/chameleon_scythe.png", "scythe"]);
+cLL.append(["css/images/imperial_pick.png", "pick"]);
+cLL.append(["css/images/1.png", "red"]);
+
+let pointer = cLL.getHead();
+
+var el = document.getElementById('swipezone');
+
 // credit: http://www.javascriptkit.com/javatutors/touchevents2.shtml
 function swipedetect(el, callback){
     var touchsurface = el,
@@ -51,26 +64,19 @@ function swipedetect(el, callback){
     }, false)
 }
 
-//USAGE:
-
-var el = document.getElementById('swipezone');
 swipedetect(el, function(swipedir){
     if(swipedir == "up"){
+
+        pointer = pointer.next;
+        console.log("Card Swipe (up)" + pointer.element);
+
         verticalSwipe = true;
         horizontalSwipe = false;
         var element = document.getElementById("venueSlider");
 
-        //Makes horizontal middle card active after swipe up.
-        //var verticalMiddleCard = document.getElementById("verticalMiddleCard");
-        //verticalMiddleCard.style.visibility = "visible";
-        //var horizontalMiddleCard = document.getElementById("horizontalMiddleCard");
-        //horizontalMiddleCard.style.visibility = "hidden";
-
-
         (function scrollUp(i) {
             element.style.top = -i + "px";
             setTimeout(function () {
-                //console.log(cardHeight);
                 if (i < cardHeight) {          // If i > 0, keep going
                     i = i + swipeVerticalSpeed;
                     scrollUp(i);       // Call the loop again, and pass it the current value of i
@@ -79,25 +85,23 @@ swipedetect(el, function(swipedir){
                     element.style.top = "0vh";
                     //verticalMiddleCard.style.visibility = "hidden";
                     //horizontalMiddleCard.style.visibility = "visible";
+                    updateCards();
                 }
             }, 20);
         })(1);
     }
     else if(swipedir == "down"){
+
+        pointer = pointer.prev;
+        console.log("Card Swipe (down)" + pointer.element);
+        
         verticalSwipe = true;
         horizontalSwipe = false;
         var element = document.getElementById("venueSlider");
 
-        //Makes horizontal middle card active after swipe up.
-        //var verticalMiddleCard = document.getElementById("verticalMiddleCard");
-        //verticalMiddleCard.style.visibility = "visible";
-        // var horizontalMiddleCard = document.getElementById("horizontalMiddleCard");
-        // horizontalMiddleCard.style.visibility = "hidden";
-
         (function scrollUp(i) {
             element.style.top = i + "px";
             setTimeout(function () {
-                //console.log(cardHeight);
                 if (i < cardHeight) {          // If i > 0, keep going
                     i = i + swipeVerticalSpeed;
                     scrollUp(i);       // Call the loop again, and pass it the current value of i
@@ -106,8 +110,46 @@ swipedetect(el, function(swipedir){
                     element.style.top = "0vh";
                     //verticalMiddleCard.style.visibility = "hidden";
                     //horizontalMiddleCard.style.visibility = "visible";
+                    updateCards();
                 }
             }, 20);
         })(1);
     }
 });
+
+function updateCards(){
+    //Initialize the first (pre-top) card with a description
+    //The card's image is not visible, so it is not necessary to initialize it.
+    let preTopCard = document.getElementById("preTopCard");
+    let preTopCardDescription = preTopCard.children[1];
+    preTopCardDescription.innerHTML = pointer.prev.prev.element[1];
+
+    //Initialize the second (top) card with a picture and description.
+    let topCard = document.getElementById("topCard");
+    let topCardImage = topCard.children[0];
+    topCardImage.src = pointer.prev.element[0];
+    let topCardDescription = topCard.children[1];
+    topCardDescription.innerHTML = pointer.prev.element[1];
+
+    //Initialize the third (middle) card with a picture and description.
+    let verticalMiddleCard = document.getElementById("verticalMiddleCard");
+    let verticalMiddleCardImage = verticalMiddleCard.children[0];
+    verticalMiddleCardImage.src = pointer.element[0];
+    let verticalMiddleCardDescription = verticalMiddleCard.children[1];
+    verticalMiddleCardDescription.innerHTML = pointer.element[1];
+
+    //Initialize the fourth (bottom) card with a picture and description.
+    let bottomCard = document.getElementById("bottomCard");
+    let bottomCardImage = bottomCard.children[0];
+    bottomCardImage.src = pointer.next.element[0];
+    let bottomCardDescription = bottomCard.children[1];
+    bottomCardDescription.innerHTML = pointer.next.element[1];
+
+    //Initialize the fifth (post-bottom) card with a picture.
+    //The card's description is not visible, so it is not necessary to initialize it.
+    let postBottomCard = document.getElementById("postBottomCard");
+    let postBottomCardImage = postBottomCard.children[0];
+    postBottomCardImage.src = pointer.next.next.element[0];
+}
+
+updateCards();
